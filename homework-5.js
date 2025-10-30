d6632f660ef6fa5624c5945cc2fcc1a736273175
reamplify <126955026+reamplify1@users.noreply.github.com>
2025-10-30 14:50:29 +0500

    fixed functions and variables

--- homework-5.js
+++ homework-5.js
@@ -1,17 +1,17 @@
 // 3. Создать функцию, которая принимает 2 параметра: город и температуру и выводит сообщение в консоль "Сейчас в X температура  — Y градусов по Цельсию"
 
-const getWeatherByCity = (city, temperature) =>
+const showCityTemperature = (city, temperature) =>
   console.log(
     `Сейчас в ${city} температура ${temperature} градусов по Цельсию`
   );
 
-getWeatherByCity("Винница", 38);
+showCityTemperature("Винница", 38);
 
 // 4. Создать переменную, которая хранит внутри себя скорость звука (гуглим). Создать функцию, которая принимает 1 параметр - скорость, внутри функции происходит проверка: если переданная скорость выше скорости звука - выводим лог "Сверхзвуковая скорость", если ниже - "Дозвуковая скорость"
 
 const SOUND_SPEED = 343;
 
-function showSpeed(speed) {
+function checkSupersonicSpeed(speed) {
   if (speed > SOUND_SPEED) {
     console.log("Сверхзвуковая скорость");
   } else if (speed < SOUND_SPEED) {
@@ -21,7 +21,7 @@ function showSpeed(speed) {
   }
 }
 
-showSpeed(228);
+checkSupersonicSpeed(228);
 
 // 5. Создать переменную, которая содержит цену какого-нибудь продукта (на ваше усмотрение). Далее создаем функцию, которая принимает 1 параметр - текущий бюджет, внутри функции происходит проверка: если бюджет превышает цену товара - выводим лог "(ваше название товара) приобретён. Спасибо за покупку!", если нет - обсчитываем разницу и выводим лог "Вам не хватает X$, пополните баланс".
 
@@ -46,7 +46,7 @@ purchaseProduct(20);
 let homework, tests, finalProject;
 
 function calcAverageGrade(homework, tests, finalProject) {
-  let averageGrade = Math.round((homework + tests + finalProject) / 3);
+  const averageGrade = Math.round((homework + tests + finalProject) / 3);
   return averageGrade;
 }
 
