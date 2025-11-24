746bfd7b5621b96f645f25dc63ad8dcf75285b54
reamplify <126955026+reamplify1@users.noreply.github.com>
2025-11-25 02:09:38 +0500

    homework-9

---
+++ homework-9.js
@@ -0,0 +1,81 @@
+const emailForm = document.querySelector("#email__form");
+const registerForm = document.querySelector("#register-form");
+const passwordRegister = document.querySelector(".user-password");
+const passwordRepeatRegister = document.querySelector(".user-repeat-password");
+const registerLabels = document.querySelectorAll('.register-form__label')
+const modal = document.querySelector('.modal');
+const modalButton = document.querySelector('.modal__button');
+const labelsModal = document.querySelectorAll('.form__modal--label');
+const formModal = document.querySelector('.form__modal');
+const modalFormError = document.querySelectorAll('.modal__form--error')
+const modalCloseButton = document.querySelector('.modal__close')
+
+
+function showDataInConsoleLog(event) {
+  event.preventDefault();
+  const form = event.target;
+  const formData = new FormData(form);
+  const data = Object.fromEntries(formData.entries());
+  console.log(data);
+  return data;
+}
+
+emailForm.addEventListener("submit", (e) => {
+  showDataInConsoleLog(e);
+});
+
+let registeredUser = null;
+
+registerForm.addEventListener("submit", (e) => {
+  const data = showDataInConsoleLog(e);
+  data.createdOn = new Date();
+
+  if (data.user_password !== data.user_repeat_password) {
+    console.log("Пароли не совпадают");
+    registerLabels[4].textContent = "Пароли не совпадают";
+    registerLabels[4].style.color = "red";
+    registerLabels[5].textContent = "Пароли не совпадают";
+    registerLabels[5].style.color = "red";
+    return; 
+  }
+
+  registeredUser = data;
+
+  openModal()
+});
+
+
+formModal.addEventListener('submit', (e) => {
+  const authorizedUser = showDataInConsoleLog(e);
+  if(registeredUser.user_name !== authorizedUser.user_name || registeredUser.user_password !== authorizedUser.user_password) {
+    modalFormError[0].textContent = 'Неверные данные';
+    modalFormError[1].textContent = 'Попробуйте еще раз';
+    modalFormError[0].style = 'color: red';
+    modalFormError[1].style = 'color: red';
+  } else {
+    modalFormError[0].textContent = '';
+    modalFormError[1].textContent = '';
+    modalFormError[0].style.color = '';
+    modalFormError[1].style.color = ''
+  }
+})
+
+modal.addEventListener('click', (e) => {
+  if(e.target === modal){
+    closeModal()
+  }
+})
+
+modalCloseButton.addEventListener('click', (e) => {
+    closeModal()
+})
+
+
+function openModal(){
+  modal.classList.add('modal__visible')
+
+}
+
+function closeModal(){
+  modal.classList.remove('modal__visible')
+}
