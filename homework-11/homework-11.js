7099c7ff62a39069ed5b9ddf448b520a9ec377c7
reamplify <126955026+reamplify1@users.noreply.github.com>
2025-12-11 16:04:00 +0500

    fixed log

--- homework-11/homework-11.js
+++ homework-11/homework-11.js
@@ -4,11 +4,12 @@ import { Lemonade } from "./Lemonade.js";
 import { Cafe } from "./Cafe.js";
 
 
-const cafe = new Cafe("starbucks", "винница 12")
+const cafe = new Cafe("starbucks", "винница, проспект космонавтов 43")
 const tea = new Tea("Чай с малиной", "0.5л", "2$", "95 градусов", "чай с жасмином")
 const coffee = new Coffee('Каппучино', "0.3", "3$", "80градусов", "миндальное молоко", "арабика")
 const lemonade = new Lemonade("лимонад", "1л", "1.5$", "-2 градуса", "1 долька лимона", "5г сахара")
 
+console.log(cafe.getCafeInfo());
 cafe.orderDrink(tea)
 tea.changeTemperature('85 градусов');
 coffee.getDrinkInfo()
